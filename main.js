ea4c2ed81a3575a557ced38b9045bd9b52f8df60
Painlessmermaid <planblocos@hotmail.com>
2022-12-26 10:17:47 -0600

    frist-branch

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.container__content__from');
+const inputEmail = document.querySelector('.container__content__from__email')
+const error = document.querySelector('.container__content__from__email--error')
+
+form.addEventListener('submit', (event) => {
+   
+    /* console.log('Se ha presionado el boton'); */
+    const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+
+if(regExp.test(inputEmail.value) == true){
+
+    inputEmail.style.border = '1px solid hsl(0, 36%, 70%)';
+
+    inputEmail.style.backgroundImage = 'url("")';
+    error.innerHTML = '';
+}else{
+    event.preventDefault();
+    inputEmail.style.border = '1px solid red';
+    inputEmail.style.backgroundImage = 'url("./images/icon-error.svg")';
+    error.innerHTML = 'Pleace provide a valid email';
+}
+   
+});
